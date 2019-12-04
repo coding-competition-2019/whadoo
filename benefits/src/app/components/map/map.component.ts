eefeb1d37751b2f255347f1502f68aa5ba145d70
stefan_nb <xknoteks@gmail.com>
2019-12-04 13:23:30 +0100

    Google Maps added.

--- benefits/src/app/components/map/map.component.ts
+++ benefits/src/app/components/map/map.component.ts
@@ -1,15 +1,41 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import {FacilityService} from '../../services/facility.service';
-
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+
+export class MapComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('mapContainer', {read: false}) gmap: ElementRef;
+
+  map: google.maps.Map;
+  // Coordinates for Prague
+  lat = 50.0755381;
+  lng = 14.4378005;
+
+  zoomLevel = 10;
+
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: this.zoomLevel,
+  };
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement,
+      this.mapOptions);
+  }
 
   constructor(private facilityService: FacilityService) {
+  }
+
+  ngAfterViewInit() {
+    this.mapInitializer();
   }
 
   ngOnInit() {
@@ -18,6 +44,7 @@ export class MapComponent implements OnInit {
   getLocation() {
     navigator.geolocation.getCurrentPosition(position => {
       console.log(position.coords.latitude, position.coords.longitude);
+
     });
   }
 }
